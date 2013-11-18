512a33553afb430b4d20f76ee2f25cc78af5fa93
Tuukka Kataja <tkataja@gmail.com>
2013-11-18 17:51:17 +0200

    Minor modifications due to schema changes in rendered data.

--- src/cljs/juvenes_menu/view.cljs
+++ src/cljs/juvenes_menu/view.cljs
@@ -8,16 +8,16 @@
   (node [:ul {:class kitchen-name}]))
 
 (defn menuitem-template [menu-item]
-  (node [:li {:class "menu-item"} menu-item]))
+  (node [:li {:class "menu-item"} (:main-dish menu-item)]))
 
 (defn restaurant-template [kitchen-name]
   (node [:div {:class "restaurant-info" :id kitchen-name}
          	[:h2 {:class "restaurant-name"} kitchen-name]]))
 
-(defn output-menu [data]
-  (let [kitchen-name (:kitchen data)]
+(defn render-menu [data]
+  (let [kitchen-name (:kitchen-name data)]
   	(-> (sel1 ".menu-container")
         (dommy/append! (restaurant-template kitchen-name)))
-    (doseq [menu-item (:menu data)]
+    (doseq [menu-item (:kitchen-menu data)]
       (-> (sel1 (str "#" kitchen-name))
           (dommy/append! (menuitem-template menu-item))))))
